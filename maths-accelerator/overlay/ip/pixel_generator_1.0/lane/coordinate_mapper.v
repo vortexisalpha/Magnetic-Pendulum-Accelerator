fd9009530204c5e04501b1bf22b8c8b6f1e4e17c
philippaflintoff2 <ptf24@ic.ac.uk>
2026-05-22 00:10:00 +0100

    CoorMap after timing

--- maths-accelerator/overlay/ip/pixel_generator_1.0/lane/coordinate_mapper.v
+++ maths-accelerator/overlay/ip/pixel_generator_1.0/lane/coordinate_mapper.v
@@ -1,3 +1,4 @@
+
 module coordinate_mapper #(
     parameter IMG_W = 160,
     parameter IMG_H = 120,
@@ -9,17 +10,16 @@ module coordinate_mapper #(
     input  signed [W-1:0]       x_max,
     input  signed [W-1:0]       y_min,
     input  signed [W-1:0]       y_max,
-    input  signed [W-1:0]       x_step,  // precomputed by CPU: (x_max-x_min)/160
-    input  signed [W-1:0]       y_step,  // precomputed by CPU: (y_max-y_min)/120
+    input  signed [W-1:0]       x_step,  
+    input  signed [W-1:0]       y_step,  
     input         [7:0]         p,
     input         [6:0]         q,
     output reg signed [W-1:0]   x0,
     output reg signed [W-1:0]   y0
 );
 
-    // DSP 1: p * x_step
     wire signed [W+8:0] p_offset_full = $signed({1'b0, p}) * x_step;
-    // DSP 2: q * y_step
+    
     wire signed [W+7:0] q_offset_full = $signed({1'b0, q}) * y_step;
 
     always @(posedge clk) begin
