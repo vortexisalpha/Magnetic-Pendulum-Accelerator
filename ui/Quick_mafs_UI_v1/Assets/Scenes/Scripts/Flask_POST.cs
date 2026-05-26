5b66784e673d4684cd1f29fd9cc6a31c1b9c3072
vortexisalpha <joshuaboshua250505@gmail.com>
2026-05-26 15:49:19 +0100

    added flask start coroutine and infinite loop structure. Post is wip

--- ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/Flask_POST.cs
+++ ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/Flask_POST.cs
@@ -1,5 +1,9 @@
+using System.Collections;
+using System.Text;
 using UnityEngine;
+using UnityEngine.Networking;
 
+[System.Serializable]
 public class ControlData 
 {
     public float dampingFactor;
@@ -9,16 +13,54 @@ public class ControlData
 }
 
 public class FlaskManager : MonoBehaviour
-{
+{   
+    private string endpoint = "controller_data";
 
     [SerializeField] GameObject dampingFactorController;
     [SerializeField] GameObject magneticStrengthController;
     [SerializeField] GameObject lengthController;
     [SerializeField] GameObject pendulumHeightController;
-    
-    ControlData data = new ControlData();
 
-    private string compileJson(float dampingFactor, float magneticStrength, float pendulumLength, float pendulumHeight) {
+    [SerializeField] private float postInterval = 0.1f; 
+
+    private ControlData data = new ControlData();
+    private float timer = 0f;
+    
+    //on start, establish url and coroutine (async function)
+    void Start()
+    {
+        string URL = "http://127.0.0.1:5000/" + endpoint;
+        StartCoroutine(PostLoop());
+    }   
+
+    //compile data every frame
+    void Update() 
+    {
+        float dampingFactor = dampingFactorController.displayValue;
+        float magneticStrength = magneticStrengthController.displayValue;
+        float pendulumLength = lengthController.displayValue;
+        float pendulumHeight = pendulumHeightController.displayValue;
+
+        string json = compileJson(dampingFactor, magneticStrength, pendulumLength, pendulumHeight);
+    }
+
+    private IEnumerator postLoop() 
+    {
+        while (true) 
+        {
+            string json = CompileJson();
+            yield return PostJson(json);
+
+            yield return new WaitForSeconds(postInterval);
+        }
+    }
+
+    private IEnumerator PostJson(string json) {
+
+    }
+
+    private string compileJson(float dampingFactor, float magneticStrength, float pendulumLength, float pendulumHeight) 
+    {
         data.dampingFactor = dampingFactor;
         data.magneticStrength = magneticStrength;
         data.pendulumLength = pendulumLength;
@@ -28,16 +70,5 @@ public class FlaskManager : MonoBehaviour
         return json;
     }
 
-    void Update() {
-        //compile data:
-        float dampingFactor = dampingFactorController.displayValue;
-        float magneticStrength = magneticStrengthController.displayValue;
-        float pendulumLength = lengthController.displayValue;
-        float pendulumHeight = pendulumHeightController.displayValue;
 
-        string json = compileJson(dampingFactor, magneticStrength, pendulumLength, pendulumHeight);
-        
-        //post to flask
-
-    }
 }
