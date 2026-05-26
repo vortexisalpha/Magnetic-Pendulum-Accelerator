47133a1fdc7175d4473b6daeae09a8eec06ce7fb
vortexisalpha <joshuaboshua250505@gmail.com>
2026-05-26 15:15:22 +0100

    git ignore remove unwanted dir

--- ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/Flask_POST.cs
+++ ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/Flask_POST.cs
@@ -1,11 +1,43 @@
 using UnityEngine;
 
+public class ControlData 
+{
+    public float dampingFactor;
+    public float magneticStrength;
+    public float pendulumLength;
+    public float pendulumHeight;
+}
+
 public class FlaskManager : MonoBehaviour
 {
-    [SerializeField] GameObject damping_factor_controller;
-    [SerializeField] GameObject magnetic_strenght_controller;
-    [SerializeField] GameObject length_controller;
-    [SerializeField] GameObject pendulum_height_controller;
 
+    [SerializeField] GameObject dampingFactorController;
+    [SerializeField] GameObject magneticStrengthController;
+    [SerializeField] GameObject lengthController;
+    [SerializeField] GameObject pendulumHeightController;
     
+    ControlData data = new ControlData();
+
+    private string compileJson(float dampingFactor, float magneticStrength, float pendulumLength, float pendulumHeight) {
+        data.dampingFactor = dampingFactor;
+        data.magneticStrength = magneticStrength;
+        data.pendulumLength = pendulumLength;
+        data.pendulumHeight = pendulumHeight;
+
+        string json = JsonUtility.ToJson(data, true);
+        return json;
+    }
+
+    void Update() {
+        //compile data:
+        float dampingFactor = dampingFactorController.displayValue;
+        float magneticStrength = magneticStrengthController.displayValue;
+        float pendulumLength = lengthController.displayValue;
+        float pendulumHeight = pendulumHeightController.displayValue;
+
+        string json = compileJson(dampingFactor, magneticStrength, pendulumLength, pendulumHeight);
+        
+        //post to flask
+
+    }
 }
