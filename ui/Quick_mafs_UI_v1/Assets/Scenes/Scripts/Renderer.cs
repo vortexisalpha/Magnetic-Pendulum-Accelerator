fd8c3e7eb431ad7b4b011ee68f71a3ce6d31fc80
vortexisalpha <joshuaboshua250505@gmail.com>
2026-05-27 16:13:45 +0100

    added get request and data handler to renderer to pick up image from flask.

--- ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/Renderer.cs
+++ ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/Renderer.cs
@@ -1,4 +1,13 @@
 using UnityEngine;
+using Newtonsoft.Json;
+
+public class ImageResponse
+{
+    public int width;
+    public int height;
+    public int bitDepth;
+    public int[][] image; 
+}
 
 public class Renderer : MonoBehaviour
 {
@@ -7,6 +16,25 @@ public class Renderer : MonoBehaviour
         
     }
 
+    IEnumerator FetchImage()
+    {
+        using (var req = UnityWebRequest.Get("http://127.0.0.1:5000/image"))
+        {
+            yield return req.SendWebRequest();
+            if (req.result != UnityWebRequest.Result.Success) yield break;
+
+            var resp = JsonConvert.DeserializeObject<ImageResponse>(req.downloadHandler.text);
+            int w = resp.width, h = resp.height;
+
+            var texCategory = new Texture2D(w, h, TextureFormat.RGBA32, false);
+            var texValue = new Texture2D(w, h, TextureFormat.RGBA32, false);
+            texCategory.filterMode = FilterMode.Point;
+            texValue.filterMode = FilterMode.Point;
+
+            var catPixels = new Color32[w * h];
+            var valPixels = new Color32[w * h];
+        }
+    }
     void Update()
     {
         
