406e807e7bbb9271a42e685284017a7b827b4224
vortexisalpha <joshuaboshua250505@gmail.com>
2026-05-27 16:26:12 +0100

    added top2 and bottom12 segregation to renderer and palette for type of magnet

--- ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/Renderer.cs
+++ ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/Renderer.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using Newtonsoft.Json;
+using Newtonsoft.Json; //needed for 2d arr json handling
 
 public class ImageResponse
 {
@@ -11,6 +11,14 @@ public class ImageResponse
 
 public class Renderer : MonoBehaviour
 {
+
+    Color32[] palette = {
+                new Color32(0,0,255,255),  //00
+                new Color32(0,255,0,255),  //01
+                new Color32(255,0,0,255),  //10
+                new Color32(0,0,0,255), //11
+            };
+
     void Start()
     {
         
@@ -24,15 +32,30 @@ public class Renderer : MonoBehaviour
             if (req.result != UnityWebRequest.Result.Success) yield break;
 
             var resp = JsonConvert.DeserializeObject<ImageResponse>(req.downloadHandler.text);
-            int w = resp.width, h = resp.height;
+            int width = resp.width;
+            height = resp.height;
 
-            var texCategory = new Texture2D(w, h, TextureFormat.RGBA32, false);
-            var texValue = new Texture2D(w, h, TextureFormat.RGBA32, false);
+            var texCategory = new Texture2D(width, height, TextureFormat.RGBA32, false);
+            var texValue = new Texture2D(width, height, TextureFormat.RGBA32, false);
             texCategory.filterMode = FilterMode.Point;
             texValue.filterMode = FilterMode.Point;
 
-            var catPixels = new Color32[w * h];
-            var valPixels = new Color32[w * h];
+            var catPixels = new Color32[width * height];
+            var valPixels = new Color32[width * height];
+
+            for (y = 0; y < height; y++){
+                for (x = 0; x < width; x++){
+                    int pixel = resp.image[y][x];
+
+                    int top2 = (pixel >> (resp.bitDepth - 2)) & 0x3; // right shifted by all but 2 and then & with ...011
+                    int bottom12 = pixel & 0xFFF;
+
+                    int bufPos = (height - y - 1) * width + x; //array buffer is inverted in unity, flip y
+
+                    catPixels[bufPos] = palette[top2];
+
+                }
+            }
         }
     }
     void Update()
