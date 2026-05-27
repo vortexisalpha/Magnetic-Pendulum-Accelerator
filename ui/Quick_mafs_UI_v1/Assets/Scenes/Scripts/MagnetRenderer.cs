9e87726783d8489a3ed30023030ae7d8785dadad
vortexisalpha <joshuaboshua250505@gmail.com>
2026-05-27 18:12:53 +0100

    added base for magnetic renderer class

--- ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/MagnetRenderer.cs
+++ ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/MagnetRenderer.cs
@@ -1,5 +1,11 @@
-using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+using UnityEngine.UI;
 
 public class MagnetCoords
 {
@@ -14,15 +20,25 @@ public class InfoResponse
 
 public class MagnetRenderer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private RawImage miniDisplay;
+    [SerializeField] private float pollIntervalSeconds = 0.5f;
+    [SerializeField] private int magnetRadius = 4;
 
-    // Update is called once per frame
-    void Update()
+    private const int W = 160;
+    private const int H = 120;
+
+    private Texture2D tex;
+    private Color32[] pixels;
+    private readonly Color32 bg = new Color32(30, 30, 40, 255);
+
+    private Color32[] palette =
     {
-        
-    }
+        new Color32(  0,   0,   0, 255),
+        new Color32(230,  60,  60, 255),
+        new Color32( 60, 200,  90, 255),
+        new Color32( 70, 130, 230, 255),
+    };
+
 }
+
+
