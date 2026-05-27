8586cebcc8995b056abb64e24dcf91a9e0a7c2b1
vortexisalpha <joshuaboshua250505@gmail.com>
2026-05-27 19:29:56 +0100

    added view manager to control multiple canvases. Buttons will be linked to show 2d and show 3d to enable and disable canvases and control orbital camera vs static camera

---
+++ ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/ViewManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ViewManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainCanvas;
+    [SerializeField] private GameObject canvas3DOverlay;
+    [SerializeField] private OrbitCamera orbitCamera;
+
+    void Start() { 
+        Show2D();
+    }
+
+    public void Show2D()
+    {
+        mainCanvas.SetActive(true);
+        canvas3DOverlay.SetActive(false);
+        orbitCamera.enabled = false;
+    }
+
+    public void Show3D()
+    {
+        mainCanvas.SetActive(false);
+        canvas3DOverlay.SetActive(true);
+        orbitCamera.enabled = true;
+    }
+}
