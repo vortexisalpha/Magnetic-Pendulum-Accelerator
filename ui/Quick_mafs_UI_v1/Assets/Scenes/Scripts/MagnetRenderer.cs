38bf7853a77eff679e12f5b7debf179ceabd4c0f
vortexisalpha <joshuaboshua250505@gmail.com>
2026-05-27 17:52:48 +0100

    added datastructure for magnet renderer

--- ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/MagnetRenderer.cs
+++ ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/MagnetRenderer.cs
@@ -1,4 +1,16 @@
 using UnityEngine;
+using System.Collections.Generic;
+
+public class MagnetCoords
+{
+    public float x;
+    public float y;
+}
+
+public class InfoResponse
+{
+    public Dictionary<string, MagnetCoords> magnets;
+}
 
 public class MagnetRenderer : MonoBehaviour
 {
