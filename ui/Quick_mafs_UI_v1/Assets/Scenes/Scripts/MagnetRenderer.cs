82efa8078739ca65f3d858f10095658ab2cd3dbc
vortexisalpha <joshuaboshua250505@gmail.com>
2026-05-27 18:27:42 +0100

    completed magnet renderer, draw circle function created using simple pythagorus

--- ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/MagnetRenderer.cs
+++ ui/Quick_mafs_UI_v1/Assets/Scenes/Scripts/MagnetRenderer.cs
@@ -96,6 +96,18 @@ public class MagnetRenderer : MonoBehaviour
 
     void DrawCircle(int cx, int cy, int r, Color32 color)
     {
+        int r2 = r * r;
+        //loop over +- delta x and +- delta y and draw if not out of bounds and not greater than r^2 (pythag)
+        for (int dy = -r; dy <= r; dy++){
+            for (int dx = -r; dx <= r; dx++){
+                if (dx * dx + dy * dy > r2) continue;
+                int x = cx + dx;
+                int y = cy + dy;
+                if (x < 0 || x >= W || y < 0 || y >= H) continue;
+                int dstY = H - 1 - y; //invert y as always
+                pixels[dstY * W + x] = color;
+            }
+        }
     }
 }
 
