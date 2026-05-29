31a24f4bb2a4c685c678e71761c3434064601284
unknown <yichankim@gmail.com>
2026-05-29 13:13:32 +0100

    updated fx_mul to span over 2 cycles

--- maths-accelerator/overlay/ip/pixel_generator_1.0/lane/fx_mul.sv
+++ maths-accelerator/overlay/ip/pixel_generator_1.0/lane/fx_mul.sv
@@ -1,32 +1,40 @@
 //SIGNED FIXED POINT MULTIPLY W SATURATION 
-// Q4.12 implementation - change W and F as required
+// Q4.14 implementation - change W and F as required
 //does a*b and puts it in c
+
+//ISSUE:
+// an issue during synthesis of the design on vivado for 8 lanes showed that
+// the design was not meeting timing constraints due to the pure
+// combinational logic in stage 2. This was resolved by pipelining the
+// design across 2 stages, with the first stage registering the raw product
+// and second stage doing the shift + saturation. This means that all stages
+// that use this fx_mul block would need an additional register stage
 
 module fx_mul #(
     parameter W = 18,
     parameter F = 14
 )(
-    input  signed [W-1:0] a,
-    input  signed [W-1:0] b,
-    output signed [W-1:0] c
+    input  logic              clk,
+    input  logic              rst,
+    input  logic signed [W-1:0] a,
+    input  logic signed [W-1:0] b,
+    output logic signed [W-1:0] c
 );
+    //saturation limits
+    localparam signed [W-1:0] SAT_MAX = {1'b0, {(W-1){1'b1}}};
+    localparam signed [W-1:0] SAT_MIN = {1'b1, {(W-1){1'b0}}};
 
-    //multiply and put it into product
-    wire signed [2*W-1:0] product;
-    assign product = a * b;
-    
-    //shift the product down by F to match correct fractoral representation
-    wire signed [2*W-1-F:0] shifted;
-    assign shifted = product >>> F;
+    //S1: multiply and put raw product into reg
+    logic signed [2*W-1:0] product_r;
+    always_ff @(posedge clk) begin
+        if (rst) product_r <= '0;
+        else      product_r <= a * b;
+    end
 
-    //define saturation limits
-    localparam signed [W-1:0] SAT_MAX = {1'b0, {(W-1){1'b1}}};  // 0x7FFFFF (01111111)
-    localparam signed [W-1:0] SAT_MIN = {1'b1, {(W-1){1'b0}}};  // 0x800000 (100000000)
-
-    //check for overflow or underflow - therefore saturate
+    //S2: shift + saturateion happens here
+    wire signed [2*W-1-F:0] shifted = product_r >>> F;
     wire overflow  = (shifted > SAT_MAX);
     wire underflow = (shifted < SAT_MIN);
+    assign c = overflow ? SAT_MAX : underflow ? SAT_MIN : shifted[W-1:0];
 
-    //assign appropriately:
-    assign c = (overflow)  ? SAT_MAX : (underflow) ? SAT_MIN : shifted[W-1:0];
 endmodule
