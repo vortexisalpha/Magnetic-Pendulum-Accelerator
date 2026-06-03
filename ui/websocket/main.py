b21db93bf64aa9a668598990e9eeeee94fd35bad
vortexisalpha <joshuaboshua250505@gmail.com>
2026-06-03 19:27:18 +0100

    added some event code for aruco for updating magnet positions, defined allowed clients for faulty reqests and added connection = true to client and called it on connect

--- ui/websocket/main.py
+++ ui/websocket/main.py
@@ -24,25 +24,40 @@ class Client:
         self.name = name
         self.connected = False
 
-    def handle_message(self, message, data):
-        raise Exception("handle_message hasnt been implemented")
+    def connect(self):
+        self.connected = True
 
 class ArUcoDetectionClient(Client):
     def __init__(self):
         super().__init__("ArUco")
-    def handle_message(self, message, data):
-        case message('')
+
+    def handle_message(self, event, message_data, mp_data):
+        match event:
+            case Event.UpdateMagnetPosition:
+                uid = message_data['uid']
+                x = float(message_data['x'])
+                y = float(message_data['y'])
+                self.update_magnet_position(uid, x, y, mp_data)
+
+    def update_magnet_position(self, uid, x, y, mp_data):
+        for magnet in mp_data.mag_list:
+            if magnet.uid == uid:
+                magnet.x = x
+                magnet.y = y
+                return
+
+    
     
 class UnityClient(Client):
     def __init__(self):
         super().__init__("Unity")
-    def handle_message(self, message, data):
+    def handle_message(self, event, message_data, mp_data):
         pass
 
 class FPGAClient(Client):
     def __init__(self):
         super().__init__("FPGA")
-    def handle_message(self, message, data):
+    def handle_message(self, event, message_data, mp_data):
         pass
 
 class ConnectionManager:
@@ -53,26 +68,41 @@ class ConnectionManager:
         self.fpga_client = FPGAClient()
         self.connections = [self.aruco_client, self.unity_client, self.fpga_client]
     
-    async def OnMessage(self, message):
+    async def on_message(self, ws):
+        message = await ws.recieve_json()
+        event = Event(message['event'])
         match message['client']:
-            case("Aruco"):
-                self.aruco_client.handle_message(message['data'], self.mp_data)
-            case("Unity"):
-                self.unity_client.handle_message(message['data'], self.mp_data)
-            case("FPGA"):
-                self.fpga_client.handle_message(message['data'], self.mp_data)
+            case "ArUco":
+                self.aruco_client.handle_message(event, message['data'], self.mp_data)
+            case "Unity":
+                self.unity_client.handle_message(event, message['data'], self.mp_data)
+            case "FPGA":
+                self.fpga_client.handle_message(event, message['data'], self.mp_data)
 
 
 connection_manager = ConnectionManager()
 
+ALLOWED_CLIENTS = ["ArUco", "Unity", "FPGA"]
 
-@app.websocket("/ws")
-async def websocket_connect(ws : WebSocket):
+@app.websocket("/ws/{client_name}")
+async def websocket_connect(ws, client_name):
     
+    if client_name not in ALLOWED_CLIENTS:
+        await ws.close()
+        return
+
+    match client_name:
+        case "ArUco":
+            connection_manager.aruco_client.connect()
+        case "Unity":
+            connection_manager.unity_client.connect()
+        case "FPGA":
+            connection_manager.fpga_client.connect()
+
     await ws.accept()
 
     while True:
-        message = await ws.recieve_json()
+        await connection_manager.on_message(ws)
 
 
 
