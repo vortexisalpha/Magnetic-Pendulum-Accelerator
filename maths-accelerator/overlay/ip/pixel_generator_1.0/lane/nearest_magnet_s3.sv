ee2677658462ac098c7e7ab835a1372337383322
dongchen-123 <chdouser@outlook.com>
2026-06-05 18:43:33 +0100

    Parameterized nearest magnet for 9 lane implementation

--- maths-accelerator/overlay/ip/pixel_generator_1.0/lane/nearest_magnet_s3.sv
+++ maths-accelerator/overlay/ip/pixel_generator_1.0/lane/nearest_magnet_s3.sv
@@ -1,41 +1,38 @@
 module nearest_magnet_s3 #(
     parameter W = 18,
     parameter F = 14,
-    parameter Q_WIDTH = 18
+    parameter Q_WIDTH = 18,
+    parameter IMG_W = 160,
+    parameter IMG_H = 120,
+    localparam ID_W = $clog2(IMG_W * IMG_H)
 )(
     input  logic clk,
     input  logic rst,
     input  logic in_valid,
-
     // Squared distances containing h^2 from Stage 3a
     input  logic [Q_WIDTH-1:0] q0,
     input  logic [Q_WIDTH-1:0] q1,
     input  logic [Q_WIDTH-1:0] q2,
-
     input  logic signed [W-1:0]       in_dx0, in_dy0, in_dx1, in_dy1, in_dx2, in_dy2,
     input  logic signed [W-1:0]       in_x, in_y,
     input  logic signed [W-1:0]       in_vx, in_vy,
     input  logic [11:0]               in_step_cnt,
-    input  logic [14:0]               in_id,
+    input  logic [ID_W-1:0]               in_id,
     input  logic [1:0]                in_settle_count,
-
     // Outputs
     output logic                      out_valid,
     output logic [1:0]                nearest_magnet_id,
     output logic [Q_WIDTH-1:0] min_q,
-
     output logic signed [W-1:0]       out_dx0, out_dy0, out_dx1, out_dy1, out_dx2, out_dy2,
     output logic signed [W-1:0]       out_x, out_y,
     output logic [11:0]               out_step_cnt,
     output logic signed [W-1:0]       out_vx, out_vy,
-    output logic [14:0]               out_id,
+    output logic [ID_W-1:0]               out_id,
     output logic [1:0]                out_settle_count,
-
     output  logic [Q_WIDTH-1:0] out_q0,
     output  logic [Q_WIDTH-1:0] out_q1,
     output  logic [Q_WIDTH-1:0] out_q2
 );
-
     // ── registered outputs ────────────────────────────────────────────────────
     always_ff @(posedge clk) begin
         if (rst) begin
@@ -44,7 +41,6 @@ module nearest_magnet_s3 #(
             out_valid <= 1'd0;
         end
         else begin
-
             //pass through values
             out_valid <= in_valid;
             out_dx0 <= in_dx0;
@@ -60,11 +56,9 @@ module nearest_magnet_s3 #(
             out_step_cnt <= in_step_cnt;
             out_id <= in_id;
             out_settle_count <= in_settle_count;
-
             out_q0 <= q0;
             out_q1 <= q1;
             out_q2 <= q2;
-
             if (in_valid) begin
                 if (q0 <= q1 && q0 <= q2) begin
                     nearest_magnet_id <= 2'd1;
@@ -85,5 +79,4 @@ module nearest_magnet_s3 #(
             end
         end
     end
-
 endmodule
